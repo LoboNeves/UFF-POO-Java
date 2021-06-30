9b61ade86ef18ee0c0cd064b605de912a60b3a7d
DanielNeves <dbenditoneves@gmail.com>
2021-06-30 13:21:20 -0300

    Reorganizado Funcionario, criado addDependente nele  e criado classe Dependente

---
+++ Dependente.java
@@ -0,0 +1,29 @@
+public class Dependente {
+    private String nome;
+    private int idade;
+
+    //Metódo Construtor
+    public Dependente (String nome, int idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    //Funções para set/get de nome
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    //Funções para set/get de  idade
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+}
--- Funcionario.java
+++ Funcionario.java
@@ -3,6 +3,48 @@ public class Funcionario {
     private String nome;
     private int matricula;
 
+    //Adição de dependentes
+    private static final int LIMITE_DEPENDENTES = 10;
+    private Dependente[] vetDep = new Dependente[10];
+
+    public void addDependente ( Dependente d) {
+        for(int i = 0; i < LIMITE_DEPENDENTES; i++) {
+            vetDep[i] = d;
+        }
+    }
+
+    //Metódo Construtor
+    public Funcionario (String nome, double salario, int matricula) {
+        this.salario = validaSalario(salario);
+        this.nome = nome;
+        this.matricula = matricula;
+    }
+
+    //Metódo Construtor para funcionários sem matrícula(a matrícula será igual a 1)
+    public static final int PRESTADOR_SERVICOS = -1;
+    public Funcionario (String nome, double salario) {
+        this(nome, salario, PRESTADOR_SERVICOS);
+    }
+
+    //Funções para set/get de nome
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    //Funções para set/get de  matricula
+    public int getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(int matricula) {
+        this.matricula = matricula;
+    }
+
+    //Funções para set/get de salario
     public double getSalario() {
         return salario;
     }
@@ -11,6 +53,7 @@ public class Funcionario {
         this.salario = validaSalario(salario);
     }
 
+    //Validação de salario
     public static final double SALARIO_MINIMO = 380.00;
     public static final double SALARIO_PRESIDENTE = 24000.0;
 
@@ -24,21 +67,5 @@ public class Funcionario {
         }
 
         return salario;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public int getMatricula() {
-        return matricula;
-    }
-
-    public void setMatricula(int matricula) {
-        this.matricula = matricula;
     }
 }
